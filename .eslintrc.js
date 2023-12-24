6795d008c0124e23b0f88393e02dc50130c0af9f
PhamDinhMinh <phamdinhminh030801@gmail.com>
2023-12-24 21:29:08 +0700

    fix: config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,17 @@
 module.exports = {
   root: true,
-  extends: [
-    '@react-native-community',
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'prettier',
-    'prettier/react',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn',
-        'react-native/no-inline-styles': 'off',
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-shadow": "off",
+        "no-undef": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "react-native/no-inline-styles": "off",
       },
     },
   ],
--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import type { PropsWithChildren } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { Icon } from '@rneui/base';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Icon} from '@rneui/base';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -36,24 +21,5 @@ function App(): JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;

3dc344d8178d9b041abb6e5a65eefd9e29652d2f
PhamDinhMinh <phamdinhminh030801@gmail.com>
2024-03-11 00:50:04 +0700

    feat: add dotenv

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 
-import {Icon} from '@rneui/base';
+import {Icon, Text} from '@rneui/base';
 import {ThemeProvider} from '@rneui/themed';
-import {theme} from 'global-style';
+import {theme} from '@/global-style';
 
 const App = () => {
   return (
@@ -13,7 +13,8 @@ const App = () => {
         backgroundColor="transparent"
         barStyle={'dark-content'}
       />
-      <Icon name="warning" type="ionicon" color={'#ffcc00'} />
+      <Icon name="warning" type="ionicon" color={'#ffcc00'} size={50} />
+      <Text>Hello anh em</Text>
     </ThemeProvider>
   );
 };

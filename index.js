7c919396f29d0cb048aa827b0469b87e53ceb761
PhamDinhMinh <phamdinhminh030801@gmail.com>
2024-05-26 16:36:45 +0700

    feat: setup track audio

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ import App from './App';
 import {name as appName} from './app.json';
 
 import './src/i18n/i18n.config';
+import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() =>
+  require('./src/track-player/service'),
+);
